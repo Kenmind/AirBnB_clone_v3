a7fb08b945fca7681f24bc729f75df03e04e55dd
Sangwani <mightyp2@gmail.com>
2022-04-18 18:12:48 +0200

    fix import error

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,8 @@
 
 from flask import Blueprint
 
+app_views = Blueprint("/api/v1", __name__, url_prefix="/api/v1/")
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -12,4 +14,3 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
 
-app_views = Blueprint("/api/v1", __name__, url_prefix="/api/v1/")
