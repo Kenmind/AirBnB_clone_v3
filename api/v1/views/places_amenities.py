ee443e16d06222ef3e2e7cbe88a9e245f8bf64f9
Sangwani <mightyp2@gmail.com>
2022-04-18 16:32:27 +0200

    place_amenities

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -4,6 +4,7 @@
 
 from api.v1.views import app_views, storage
 from flask import abort, jsonify
+from os import getenv
 
 
 @app_views.route("/places/<place_id>/amenities", methods=['GET'],
@@ -14,20 +15,21 @@ def get_amenity_by_place(place_id):
         returns: json of all amenities
     """
     place_obj = storage.get("Place", str(place_id))
+
+    amenities_list = []
+
     if place_obj is None:
         abort(404)
 
-    review_list = []
+    for obj in place_obj.amenities:
+        amenities_list.append(obj.to_dict())
 
-    for obj in place_obj.reviews:
-        review_list.append(obj.to_dict())
-
-    return jsonify(review_list)
+    return jsonify(amenities_list)
 
 
 @app_views.route("/places/<place_id>/amenities/amenity_id",
                  methods=['DELETE'], strict_slashes=False)
-def delete_amenity(place_id, amenity_id):
+def delete_amenity_from_place(place_id, amenity_id):
     """ Deletes a Amenity object to a place
         if place_id is not linked to a place raise 404 error
         if amenity_id is not linked to any amenity raise a 404 error
@@ -35,15 +37,28 @@ def delete_amenity(place_id, amenity_id):
         raise a 404 error.
         returns: empty dict with 200 status code
     """
-    review_obj = storage.get("Review", str(review_id))
+    place_obj = storage.get("Place", str(place_id))
+    amenity_obj = storage.get("Amenity", str(amenity_id))
+    found_amenity = 0
 
-    if review_obj is None:
+    if not place_obj or not amenity_obj:
         abort(404)
 
-    storage.delete(review_obj)
-    storage.save()
-
-    return jsonify({})
+    for obj in place_obj.amenities:
+        if str(obj.id) == amenity_id:
+            if getenv("HBNB_TYPE_STORAGE") == "db":
+                place_obj.amenities.remove(obj)
+            else:
+                place_obj.amenity_ids.remove(obj.id)
+            place_obj.save()
+            found_amenity = 1
+            break
+    if found_amenity == 0:
+        abort(404)
+    else:
+        res = jsonify({})
+        res.status_code = 201
+        return res
 
 
 @app_views.route("/places/<place_id>/amenities/amenity_id",
@@ -58,21 +73,28 @@ def link_amenity_to_place(place_id, amenity_id):
             return the Amenity wit status code 200
         :returns: the Amenity with status code 201
     """
-    review_json = request.get_json(silent=True)
-    if review_json is None:
-        abort(400, "Not a JSON")
+    place_obj = storage.get("Place", str(place_id))
+    amenity_obj = storage.get("Amenity", str(amenity_id))
+    found = None
 
-    if not storage.get("Place", str(place_id)):
+    if not place_obj or not amenity_obj:
         abort(404)
 
-    if "name" not in review_json:
-        abort(400, "Missing name")
+    for obj in place_obj.amenities:
+        if str(obj.id) == amenity_id:
+            found = obj
+            break
+    if found is not None:
+        return jsonify(found.to_dict())
 
-    review_json["place_id"] = place_id
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        place_obj.amenities.append(amenity_obj)
+    else:
+        place_obj.amenities = amenity_obj
 
-    new_review = Review(**review_json)
-    new_review.save()
-    resp = jsonify(new_review.to_dict())
-    resp.status_code = 201
+    place_obj.save()
 
-    return resp
+    response = jsonify(amenity_obj.to_dict())
+    response.status_code = 201
+
+    return response
